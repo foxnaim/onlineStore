20e4c553039b9ef8c6f823d544d0fdcd9dc53f24
Yan <mksimantipev2.2.8@gmail.com>
2025-02-04 15:29:45 +0500

    modified:   src/App.jsx 	modified:   src/main.jsx 	new file:   src/pages/Cart.jsx 	new file:   src/pages/Login.jsx 	new file:   src/pages/Orders.jsx 	new file:   src/pages/PlaceOrder.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home.jsx';
+import Collection from './pages/Collection.jsx';
+import About from './pages/About.jsx';
+import Content from './pages/Content.jsx';  
+import Product from './pages/Product.jsx';
+import Cart from './pages/Cart.jsx';
+
 
 export default function App() {
   return (
     <React.Fragment>
-      
+      <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/collection' element={<Collection/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/contact' element={<Content/>}/>
+          <Route path='/product/:productId' element={<Product/>}/>
+          <Route path='/cart' element={<Cart/>}/>
+        </Routes>
+      </div>
     </React.Fragment>
   )
 }
