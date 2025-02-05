d33e15723eb32439cf19dcc5860b979b60807c01
Yan <mksimantipev2.2.8@gmail.com>
2025-02-05 13:26:05 +0500

    modified:   src/components/LatestCollection.jsx

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -1,15 +1,21 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { ShopContext } from '../context/ShopContext';
 import Title from './Title';
 
 const LatestCollection = () => {
  const { products } = useContext(ShopContext);
- 
+ const [latestProducts, setlatestProducts] = useState([]);
+ useEffect(() =>{
+
+ },[])
  return (
    <div className='my-10'>
      <div className='text-center py-8 text-3xl'>
        <Title text1={"LATEST"} text2={"COLLECTIONS"} />
+       <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+       Recusandae nisi magnam dolore neque molestiae consectetur quam eveniet fuga ipsam optio saepe dignissimos temporibus, assumenda voluptatum, odit repudiandae voluptatem nam natus.</p>
      </div>
+
    </div>
  );
 };
